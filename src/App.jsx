3b3c340d58f6b7c019ca6b847a011c05ef7e3a5c
Bruno-DarkSouler <brunofornasar@gmail.com>
2026-04-28 00:30:13 -0300

    Estilos en modulos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {  } from "../node_modules/socket.io/client-dist/socket.io";
+import styles from "./css/general.module.css";
 
 const socket = io();
 socket.on('Hola a', (gapura_pico, calificacion) => {
@@ -10,7 +11,23 @@ function App() {
     <>
     <div>
       <div id="datos_dron">
-        
+        Datos
+      </div>
+      <div id="mapas_drones">
+        <div className="{styles.grilla7030}">
+          <div>Mapa</div>
+        </div>
+        <div className={styles.scroll_horizontal}>
+          <div className={styles.mapa}> 
+            Mapa
+          </div>
+          <div className={styles.mapa}> 
+            Mapa
+          </div>
+          <div className={styles.mapa}> 
+            Mapa
+          </div>
+        </div>
       </div>
     </div>
     </>
